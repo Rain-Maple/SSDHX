e1717cc4a9135735f34c19402b40a714392debe4
Rain-Maple <83900858+Rain-Maple@users.noreply.github.com>
2025-04-23 15:47:31 +0800

    更新 password-generator.js

--- js/password-generator.js
+++ js/password-generator.js
@@ -51,7 +51,7 @@ class PasswordGenerator extends HTMLElement {
         .close-btn {
           background: none;
           border: none;
-          font-size: 26px;
+          font-size: 28px;
           cursor: pointer;
           color: #999;
           padding: 0;
