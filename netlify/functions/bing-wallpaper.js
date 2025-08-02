ea2d327b4aba4cb871645992c41d0deb6b529631
RainMaple <sonsq@msn.com>
2025-08-02 14:28:33 +0800

    壁纸更新

--- netlify/functions/bing-wallpaper.js
+++ netlify/functions/bing-wallpaper.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
     const cnDate = new Date(Date.now() + 8 * 3600 * 1000).toISOString().split('T')[0];
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=zh-cn`; // 中国
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=ja-jp`; // 日本
-    // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=en-gb`; // 英国
+    const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=en-gb`; // 英国
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=en-us`; // 美国
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=es-es`; // 西班牙
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=fr-fr`; // 法国
@@ -17,7 +17,7 @@ exports.handler = async (event, context) => {
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=es-cl`; // 智利
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=en-ca`; // 加拿大
     // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=en-au`; // 澳大利亚
-    const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=pt-br`; // 巴西
+    // const apiUrl = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&_=${cnDate}&mkt=pt-br`; // 巴西
 
     const apiRes = await axios.get(apiUrl, {
       headers: { 'User-Agent': 'Mozilla/5.0' }
